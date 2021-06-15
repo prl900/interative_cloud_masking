fc4fbfd06f2466ab301893ebbbe4a271c7e1fb06
prl900 <pablo.larraondo@anu.edu.au>
2021-06-15 23:40:14 +1000

    simplified color drawing

---
+++ alt_main.py
@@ -0,0 +1,230 @@
+import plotly.express as px
+import dash
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import json
+from skimage import io as skio
+import io
+import base64
+import PIL.Image
+import pickle
+from time import time
+
+DEFAULT_STROKE_WIDTH = 3  # gives line width of 2^3 = 8
+
+DEFAULT_IMAGE_PATH = "assets/segmentation_img.jpg"
+
+# the number of different classes for labels
+NUM_LABEL_CLASSES = 5
+DEFAULT_LABEL_CLASS = 0
+class_label_colormap = ["#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"]
+class_labels = list(range(NUM_LABEL_CLASSES))
+# we can't have less colors than classes
+assert NUM_LABEL_CLASSES <= len(class_label_colormap)
+
+# Font and background colors associated with each theme
+text_color = {"dark": "#95969A", "light": "#595959"}
+card_color = {"dark": "#2D3038", "light": "#FFFFFF"}
+
+
+def class_to_color(n):
+    return class_label_colormap[n]
+
+
+def color_to_class(c):
+    return class_label_colormap.index(c)
+
+
+img = skio.imread(DEFAULT_IMAGE_PATH)
+
+external_stylesheets = [dbc.themes.BOOTSTRAP, "assets/segmentation-style.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+app.title = "Interactive image segmentation based on machine learning"
+
+
+def make_default_figure(
+    stroke_color=class_to_color(DEFAULT_LABEL_CLASS),
+    stroke_width=DEFAULT_STROKE_WIDTH,
+    shapes=[],
+):
+    fig = px.imshow(img)
+    fig.update_layout(
+        {
+            "dragmode": "drawopenpath",
+            "shapes": shapes,
+            "newshape.line.color": stroke_color,
+            "newshape.line.width": stroke_width,
+            "margin": dict(l=0, r=0, b=0, t=0, pad=4),
+        }
+    )
+    return fig
+
+
+# Image Segmentation
+segmentation = [
+    dbc.Card(
+        id="segmentation-card",
+        children=[
+            dbc.CardHeader("Viewer"),
+            dbc.CardBody(
+                [
+                    # Wrap dcc.Loading in a div to force transparency when loading
+                    html.Div(
+                        id="transparent-loader-wrapper",
+                        children=[
+                            dcc.Loading(
+                                id="segmentations-loading",
+                                type="circle",
+                                children=[
+                                    # Graph
+                                    dcc.Graph(
+                                        id="graph",
+                                        figure=make_default_figure(),
+                                        config={
+                                            "modeBarButtonsToAdd": [
+                                                "drawrect",
+                                                "drawopenpath",
+                                                "eraseshape",
+                                            ]
+                                        },
+                                    ),
+                                ],
+                            )
+                        ],
+                    ),
+                ]
+            ),
+        ],
+    )
+]
+
+# sidebar
+sidebar = [
+    dbc.Card(
+        id="sidebar-card",
+        children=[
+            dbc.CardHeader("Tools"),
+            dbc.CardBody(
+                [
+                    html.H6("Label class", className="card-title"),
+                    # Label class chosen with buttons
+                    html.Div(
+                        id="label-class-buttons",
+                        children=[
+                            dbc.Button(
+                                "%2d" % (n,),
+                                id={"type": "label-class-button", "index": n},
+                                style={"background-color": class_to_color(c)},
+                            )
+                            for n, c in enumerate(class_labels)
+                        ],
+                    ),
+                    html.Hr(),
+                    dbc.Form(
+                        [
+                            dbc.FormGroup(
+                                [
+                                    dbc.Label(
+                                        "Width of annotation paintbrush",
+                                        html_for="stroke-width",
+                                    ),
+                                    # Slider for specifying stroke width
+                                    dcc.Slider(
+                                        id="stroke-width",
+                                        min=0,
+                                        max=6,
+                                        step=0.1,
+                                        value=DEFAULT_STROKE_WIDTH,
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+        ],
+    ),
+]
+
+meta = [
+    html.Div(
+        id="no-display",
+        children=[
+            # Store for user created masks
+            # data is a list of dicts describing shapes
+            dcc.Store(id="masks", data={"shapes": []}),
+        ],
+    ),
+]
+
+app.layout = html.Div(
+    [
+        dbc.Container(
+            [
+                dbc.Row(
+                    id="app-content",
+                    children=[dbc.Col(segmentation, md=8), dbc.Col(sidebar, md=4)],
+                ),
+                dbc.Row(dbc.Col(meta)),
+            ],
+            fluid=True,
+        ),
+    ]
+)
+
+
+@app.callback(
+    [
+        Output("graph", "figure"),
+        Output("masks", "data"),
+    ],
+    [
+        Input("graph", "relayoutData"),
+        Input(
+            {"type": "label-class-button", "index": dash.dependencies.ALL},
+            "n_clicks_timestamp",
+        ),
+        Input("stroke-width", "value"),
+    ],
+    [State("masks", "data"),],
+)
+def annotation_react(
+    graph_relayoutData,
+    any_label_class_button_value,
+    stroke_width_value,
+    masks_data,
+):
+    cbcontext = [p["prop_id"] for p in dash.callback_context.triggered][0]
+    if cbcontext == "graph.relayoutData":
+        if "shapes" in graph_relayoutData.keys():
+            masks_data["shapes"] = graph_relayoutData["shapes"]
+        else:
+            return dash.no_update
+    stroke_width = int(round(2 ** (stroke_width_value)))
+    # find label class value by finding button with the most recent click
+    if any_label_class_button_value is None:
+        label_class_value = DEFAULT_LABEL_CLASS
+    else:
+        label_class_value = max(
+            enumerate(any_label_class_button_value),
+            key=lambda t: 0 if t[1] is None else t[1],
+        )[0]
+
+    fig = make_default_figure(
+        stroke_color=class_to_color(label_class_value),
+        stroke_width=stroke_width,
+        shapes=masks_data["shapes"],
+    )
+    fig.update_layout(uirevision="segmentation")
+    return (
+        fig,
+        masks_data,
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
